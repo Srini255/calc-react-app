34be6a597bc46928cbe345dc470f84f901363738
srini255 <srinitamil17@gmail.com>
2024-09-23 23:40:11 +0530

    Calculator App React Migration

---
+++ src/app/App.js
@@ -0,0 +1,18 @@
+const App = () => {
+    return ( 
+        <Header />
+     );
+}
+ 
+export default App;
+
+function Header(){
+    return (
+        <header>
+            <span className="logo"></span>
+            <h1>Calculator Web App</h1>
+            <span>
+            </span>
+        </header>
+    )
+}
