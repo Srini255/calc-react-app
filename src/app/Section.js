55da4e863bfae9fd7446bf37f90fa5919f09507e
srini255 <srinitamil17@gmail.com>
2024-09-26 23:52:57 +0530

    Calculator ui
    Calculator UI is configured with react js

--- src/app/Section.js
+++ src/app/Section.js
@@ -2,8 +2,53 @@ function Section() {
     return ( 
         <section>
              <div className="img"></div>
-             <div className="calc"></div>
+             <div className="calc">
+             <table>
+               <Displaybtn/>
+               <tr>
+               <Rows text={1} />
+               <Rows text={2} />
+               <Rows text={3} />
+               <Rows text={'+'} />
+               </tr>
+               <tr>
+               <Rows text={4} />
+               <Rows text={5} />
+               <Rows text={6} />
+               <Rows text={'-'} />
+               </tr>
+               <tr>
+               <Rows text={7} />
+               <Rows text={8} />
+               <Rows text={9} />
+               <Rows text={'*'} />
+               </tr>
+               <td></td>
+               <Rows text={0} />
+               <Rows text={'/'} />
+               <Rows id={'Equal'} text={'='} />
+             </table>
+             </div>
         </section>
+     );
+}
+
+function Displaybtn(){
+     return (
+          <tr>
+          <td colSpan="3">
+               <input type="text" className="dis" id="display" />
+          </td>
+          <td><button className="button" id="clear">C</button></td>
+          </tr>
+     );
+}
+
+function Rows(props){
+     return(
+          <td>
+               <button className="button" id={props.id?props.id:"number"}>{props.text}</button>
+          </td>
      );
 }
 
